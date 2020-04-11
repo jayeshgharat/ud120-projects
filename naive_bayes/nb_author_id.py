34fb56b6cead1e066561058c34b3ff9a5db5fa97
Jayesh Gharat <jayeshghrt7@gmail.com>
2020-04-11 17:08:34 +0530

    Error Importing Sklearn Cross_Validation is fixed in tools\email_preprocess.py
    Error Importing Sklearn Cross_Validation is fixed in tools\email_preprocess.py
    
    Soln:
    from sklearn.model_selection import train_test_split
    
    and below call train_test_split

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,6 +27,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+### import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+### create classifier
+clf = GaussianNB()
+
+### fit the classifier on the training features and labels
+clf.fit(features_train, labels_train)
+    
+#submitAccuracyained classifier to predict labels for the test features
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 
 #########################################################
 
